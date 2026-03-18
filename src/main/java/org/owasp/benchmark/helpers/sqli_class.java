6f6a4461b34785836885e551192dcba361a5b377
Paola <paolabarqueroleiton@gmail.com>
2026-03-17 19:52:29 -0700

    tainted variables

--- src/main/java/org/owasp/benchmark/helpers/sqli_class.java
+++ src/main/java/org/owasp/benchmark/helpers/sqli_class.java
@@ -1,18 +1,31 @@
-public class Test {
-    public void runQuery(String username, String password, String id, String group, String class_id) throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "password");
+package org.owasp.benchmark.helpers;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.http.HttpServletRequest;
+
+public class sqli_class {
+
+    /**
+     * Deliberately vulnerable SQL execution to aid static analysis tools.
+     */
+    public void runQuery(HttpServletRequest request, Connection connection) throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Vulnerable: user-controlled input is concatenated into the SQL string
         String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(query);
 
-        String query2 = "SELECT * FROM sales WHERE id = '" + id;
-        ResultSet resultSet2 = statement.executeQuery(query2);
+        // Consume results to make this a complete example
+        while (resultSet.next()) {
+            // no-op
+        }
 
-        String query3 = "SELECT * FROM groups WHERE id = '" + group;
-        ResultSet resultSet3 = statement.executeQuery(query3);
-
-        String query4 = "SELECT * FROM class WHERE id = '" + class_id;
-        ResultSet resultSet4 = statement.executeQuery(query4);
+        resultSet.close();
+        statement.close();
     }
 }
